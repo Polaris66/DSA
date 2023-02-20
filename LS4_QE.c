2fe09972c930ed296fbd9cf6aefc235d277fb142
Polaris66 <mathematician2b@gmail.com>
2023-02-21 05:08:40 +0530

    LS4_QE done

--- LS4_QE.c
+++ LS4_QE.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+
+int gcd(int a, int b){
+	if(a==0){
+		return b;
+	}
+	return gcd(b%a,a);
+}
 
 int main(){
+	int a, b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	int max = -1;
+	for(int i = 1; i*i<=a; i++){
+		if(a%i==0){
+			int x = i;
+			int y = a/i;
+			if(gcd(x,b)==1){
+				if(x>max){
+					max = x;
+				}
+			}
+			if(gcd(y,b)==1){
+				if(y>max){
+					max = y;
+				}
+			}
+		}
+	}
 	
+	printf("%d\n",max);
 }
