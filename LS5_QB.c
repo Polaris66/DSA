6421c16fff69ed223db6787d29274532fc2c9af9
Polaris66 <mathematician2b@gmail.com>
2023-03-02 07:25:19 +0530

    LS5_QB done

--- LS5_QB.c
+++ LS5_QB.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
+long long death(int head, int remaining, int step, int left){
+	if(remaining==1){
+		return head;
+	}
+	if(left==1 || (left==0 && remaining%2==1)){
+		head+=step;
+	}
+	if(left){
+		left = 0;
+	}
+	else{
+		left = 1;
+	}
+	head = death(head,remaining/2,2*step,left);
+	return head;
+}
+
 int main(){
 	long long int n;
 	scanf("%lld",&n);
 	
+	printf("%lld\n",death(1,n,1,1));
 }
