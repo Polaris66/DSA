11cb2c39b5669fab8674d6c837af95f6be570209
Polaris66 <mathematician2b@gmail.com>
2023-03-06 18:43:54 +0530

    LS6_QI done

--- LS6_QI.cpp
+++ LS6_QI.cpp
@@ -19,12 +19,35 @@ int main(){
 			}
 		}
 		else{
-			temp.push_back(absPath[i]);
+			if(absPath[i] == '.'){
+				if(i+1<n && absPath[i+1]=='.'){
+					dirs.pop();
+					i++;
+				}
+				else{
+					temp.clear();	
+				}
+			}
+			else{
+				temp.push_back(absPath[i]);
+			}
 		}
 	}
 	if(!temp.empty()){
 		dirs.push(temp);
 		temp.clear();
+	}
+	stack<string> newdirs;
+	while(!dirs.empty()){
+		newdirs.push(dirs.top());
+		dirs.pop();
+	}
+	if(newdirs.empty()){
+		cout<<'/';
+	}
+	while(!newdirs.empty()){
+		cout<<'/'<<newdirs.top();
+		newdirs.pop();
 	}
 	cout<<endl;
 }
