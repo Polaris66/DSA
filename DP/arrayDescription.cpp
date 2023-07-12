fe0a0c1d6857b4e9e30bb416f1136296c7b777b5
Polaris66 <64375061+Polaris66@users.noreply.github.com>
2023-07-12 19:20:19 +0530

    Array Destination some test cases passed

--- DP/arrayDescription.cpp
+++ DP/arrayDescription.cpp
@@ -4,7 +4,38 @@ using namespace std;
 #define mod 1000000007
 
 int recursion(vector<int> a, int m, int i, int n){
-    
+    if(i==n){
+        return 1;
+    }
+    int ans = 0;
+    if(a[i]!=0){
+        if (i == 0)
+        {
+            ans += recursion(a,m,i+1,n) % mod;
+        }
+        else if (abs(a[i - 1] - a[i]) <= 1)
+        {
+            ans += recursion(a, m, i + 1,n) % mod;
+        }
+    }
+    else{
+        for(int j = 1; j <= m; j++){
+            // Check If Possible
+        if(i==0){
+            a[i] = j; 
+            ans += recursion(a,m,i+1,n) % mod;
+        }
+        else if ((abs(a[i - 1] - j) <= 1)){
+            a[i] = j;
+            ans += recursion(a, m, i + 1, n) % mod;
+        }
+        }
+    }
+    return ans%mod;
+}
+
+int tabulation(){
+    return 1;
 }
 
 int main(){
@@ -16,6 +47,5 @@ int main(){
         cin>>x;
     }
 
-    cout<<recursion(a,m,n-1,n)<<endl;
-
+    cout<<recursion(a,m,0,n)<<endl;
 }
